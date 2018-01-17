de5f994f8a5810222ed621eb73781d256fd4a34f
Shreyas Pawar <35517826+shreypawar@users.noreply.github.com>
2018-01-17 12:05:45 +0530

    updateform1.cs
    changed comment on line 5

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,7 +2,7 @@
 {
     partial class Form1
     {
-        /// <summary>
+        /// <summary> //hi chetan
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
